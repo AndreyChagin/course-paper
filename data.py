ded9ce71d562c8529fe6003b9b35292c19e3b612
AndreyChagin <volchonok1337@rambler.ru>
2022-05-05 18:09:59 +0300

    update

--- data.py
+++ data.py
@@ -3,17 +3,20 @@ import random
 with open('result.json', encoding='utf-8') as file:
     data = json.load(file)
 
-set_tags = set()
+
+lst_tags = list()
 
 for y in data:
     for x in y['Tags'].strip().split(' /  '):
-        set_tags.add(x)
+        if x in lst_tags:
+            continue
+        else:
+            lst_tags.append(x)
 
-set_tags = list(set_tags)
 
-set_tags = [set_tags[i:i + 4] for i in range(0, len(set_tags), 4)]
-set_tags = [' / '.join(item) for item in set_tags]
-set_tags = '\n'.join(set_tags)
+lst_tags = [lst_tags[i:i + 4] for i in range(0, len(lst_tags), 4)]
+lst_tags = [' / '.join(item) for item in lst_tags]
+lst_tags = '\n'.join(lst_tags)
 
 lst = [i for i in range(950)]
 random.shuffle(lst)
