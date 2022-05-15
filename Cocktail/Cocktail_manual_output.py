5da28aca6f04be8b3bbf82b2b645cf916e2f245d
AndreyChagin <volchonok1337@rambler.ru>
2022-05-15 03:32:23 +0300

    update

--- Cocktail/Cocktail_manual_output.py
+++ Cocktail/Cocktail_manual_output.py
@@ -1,7 +1,7 @@
 class OutputManual:
     def __init__(self, x: dict):
-        self.output_manual_dict = x
+        self.__output_manual_dict = x
         self.output_manual = self.__output()
 
     def __output(self):
-        return "\n".join([str(key) + ". " + str(value) for key, value in self.output_manual_dict.items()])
+        return "\n".join([str(key) + ". " + str(value) for key, value in self.__output_manual_dict.items()])
